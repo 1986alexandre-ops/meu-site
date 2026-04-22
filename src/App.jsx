727452782399d78f77a38e5fa799aea590df05f8
1986alexandre-ops <1986alexandre@gmail.com>
2026-04-21 22:00:10 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  const [msg, setMsg] = useState('Carregando...')
+
+  useEffect(() => {
+    setMsg('Dashboard iniciado 🚀')
+  }, [])
+
   return (
-    <h1>Site rodando 🚀</h1>
+    <div style={{ padding: 20 }}>
+      <h1>{msg}</h1>
+      <p>Seu sistema está online no Vercel</p>
+    </div>
   )
 }
 
